07cbefd14544d1b3d067ba597cf33769c5286a5f
dhamaniasad <dhamaniasad@gmail.com>
2014-11-05 19:28:27 +0530

    .

--- mb.py
+++ mb.py
@@ -1,23 +1,29 @@
 import requests
 import html2text
 import filecmp
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
 
-r = requests.get("http://gillesfabio.github.io/meteor-bookmarks/")
-h = html2text.HTML2Text()
-h.ignore_images = True
-s = r.text.encode('ascii', 'ignore').decode('ascii')
-mdown = h.handle(s)
-f3 = open('sourcediff.mdown', 'w+')
-print >> f3, mdown
-with open('sourcediff.mdown') as no2:
-    s2 = no2.read()
-with open('source.mdown') as no1:
-    s1 = no1.read()
-if s2 == s1:
-    pass
-else:
-    print "Different"
-"""
-f2 = open('source.mdown', 'w+')
-print >> f2, mdown
-"""
+
+def run():
+    r = requests.get("http://gillesfabio.github.io/meteor-bookmarks/")
+    h = html2text.HTML2Text()
+    h.ignore_images = True
+    s = r.text.encode('ascii', 'ignore').decode('ascii')
+    mdown = h.handle(s)
+    f3 = open('sourcediff.md', 'w+')
+    print >> f3, mdown
+    f3.close()
+    if filecmp.cmp('sourcediff.md', 'source.md') is False:
+        os.remove('source.md')
+        f2 = open('source.md', 'w+')
+        print >> f2, mdown
+    os.remove('sourcediff.md')
+
+apsched = BackgroundScheduler()
+apsched.add_job(run, 'interval', seconds=86400)
+apsched.start()
+
+while True:
+    time.sleep(86000)
